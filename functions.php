d47c50e39e59fb2b83fe5d29b09ae134b7c13a3c
Bruno Canini <brunocanini1994@gmail.com>
2023-06-11 16:48:27 +0200

    boostrap

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function randomPassword($lunghezza) {
     $data = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz$%&/_()';
 
     if( $lunghezza <= 0){
-        return "Inserisci i campi";
+        return "Nessun parametro valido inserito";
     } else{
         return substr(str_shuffle($data), 0, $lunghezza );
     }
--- index.php
+++ index.php
@@ -10,25 +10,46 @@ include 'functions.php'
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>php-strong-password-generator</title>
 </head>
-<body>
+<body class="bg-primary p-5">
 
-    <main>
+    <header class="container-fluid">
+        <h1 class="text-center text-white-50">Strong Password Generator</h1>
+        <h2 class="text-center text-white">Genera una password sicura</h2>
+    </header>
 
-        <h1>Scegliere La Lunghezza Della Pasword Da Generare</h1>
+    <main class="container-fluid mt-4">
 
-        <form action="index.php">
-            <input type="number" min="8" max="10" name="lunghezza">
-            <button type="submit">VAI</button>
-        </form>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col p-4 bg-light">
+                    <?php if(isset($_GET["lunghezza"]))  { ?>
+                    <?php echo randomPassword($lunghezza) ?>
+                    <?php } ?>
+                </div>
+            </div> 
+        </div>
 
-        <?php if(isset($_GET["lunghezza"]))  { ?>
-            <?php echo randomPassword($lunghezza) ?>
-        <?php } ?>
+        <div class="container-fluid mt-4">
+            <div class="row">
+                <div class="col-6  p-4 bg-light">
+                    <p>Lunghezza password</p>
+                </div>
 
+                <div class="col-6  p-4 bg-light">
+                    <form action="index.php">
+                        <input type="number"  class="form-control" min="10" max="20" name="lunghezza">
+                        <button type="submit" class="btn btn-primary mt-3">VAI</button>
+                    </form> 
+                </div>
+            </div>
+        </div>
 
     </main>
-    
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
